7785e4e35f384bbead0117fc8d24b0a37e2fdeed
Samar <52859266+SamarKamboj@users.noreply.github.com>
2021-10-27 20:45:22 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -11,34 +11,39 @@ word_length = len(chosen_word)
 lives = 6
 
 # Game starts from here
-print(art1)
-display = []  # --> Empty List
-for x in range(word_length):
-    display.append("_")  # Adding blanks in list
 
-end_of_game = False
-while end_of_game == False:
 
-    #Check guessed letter
-    guess = input("Guess a letter: ").lower()
-    for y in display:
-        if guess == y:
-            print("You have already guessed the word. Try another word.")
-    if guess not in chosen_word:
-        lives -= 1
-        print("You guessed wrong word.")    
-    else:
-        for x in range(word_length):
-            letter = chosen_word[x]
-            if letter == guess:
-                display[x] = letter 
-    print(f"{' '.join(display)}")   #Join all the elements in the list and turn it into a String.
-    print(art2[lives])
-    
-    if lives == 0:
-        end_of_game = True
-        print("The word was: " + chosen_word.upper())
-        print("Game over")
-    elif "_" not in display:
-        end_of_game = True
-        print("You win.")
+exit = True
+while exit == True: 
+    print(art1)
+    display = []  # --> Empty List
+    for x in range(word_length):
+        display.append("_")  # Adding blanks in list 
+
+    end_of_game = False
+    while end_of_game == False:
+
+        #Check guessed letter
+        guess = input("Guess a letter: ").lower()
+        if guess not in chosen_word:
+            lives -= 1
+            print("You guessed wrong word.")
+        else:
+            for x in range(word_length):
+                letter = chosen_word[x]
+                if letter == guess:
+                    display[x] = letter 
+        print(f"{' '.join(display)}")   #Join all the elements in the list and turn it into a String.
+        print(art2[lives])
+        
+        if lives == 0:
+            end_of_game = True
+            print("The word was: " + chosen_word.upper())
+            print("Game over")
+        elif "_" not in display:
+            end_of_game = True
+            print("You win.")
+
+    last = int(input("Type 0 to Exit or Any number to Play Again.\n"))
+    if last == 0:
+        exit = False
