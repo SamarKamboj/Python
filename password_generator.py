5065d91b00987d6076aabe04d4139b042a2bf4d9
Samar <52859266+SamarKamboj@users.noreply.github.com>
2021-10-27 20:45:57 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,50 +2,33 @@
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
+pg = True
+while pg == True:
+    print("Welcome to the PyPassword Generator!")
 
-# Eazy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+    nr_letters= int(input("How many letters would you like in your password?\n")) 
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-let = ''
-for x in range(0, nr_letters):
-  let += letters[random.randint(0, len(letters)-1)]
+    let = []
+    for x in range(0, nr_letters):
+        let += random.choice(letters)  # Angela's method
 
-num = ''
-for x in range(0, nr_numbers):
-  num += numbers[random.randint(0, len(numbers)-1)]
+    for y in range(0, nr_numbers):
+        let += random.choice(numbers)  # Angela's method
 
-sym = '' 
-for x in range(0, nr_symbols):
-  sym += symbols[random.randint(0, len(symbols)-1)]
+    for z in range(0, nr_symbols):
+        let += random.choice(symbols)  # Angela's method
 
-print("Easy password: " + let + num + sym)
+    random.shuffle(let)
 
-# Hard Level - Order of characters randomised:
-# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+    password = ''
+    for a in let:
+        password += a
+    print(f"Your Password is: {password}")
 
-let = []
-for x in range(0, nr_letters):
-  # let += letters[random.randint(0, len(letters)-1)]  # My method
-  let += random.choice(letters)  # Angela's method
-
-for y in range(0, nr_numbers):
-  # let += numbers[random.randint(0, len(numbers)-1)]  # My method
-  let += random.choice(numbers)  # Angela's method
-
-for z in range(0, nr_symbols):
-  # let += symbols[random.randint(0, len(symbols)-1)]  # My method
-  let += random.choice(symbols)  # Angela's method
-
-random.shuffle(let)
-
-password = ''
-for a in let:
-  password += a
-
-print("Hard password: " + password)
+    end = int(input("Type 0 to Exit or Any number to Generate Password Again\n"))
+    if end == 0:
+        pg = False
