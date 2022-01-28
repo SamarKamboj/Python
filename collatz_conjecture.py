5d0b77b2a8b89ab6c1d46ab6151fb774bcaa1024
Samar <52859266+SamarKamboj@users.noreply.github.com>
2022-01-28 19:01:03 +0530

    Add files via upload

---
+++ collatz_conjecture.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+x = [*range(1, 101)]
+y = []
+
+for i in x:
+    a = i
+    steps = 0
+
+    while a != 1:
+        if a%2 == 0:
+            a /= 2
+            steps += 1
+        elif a%2 != 0:
+            a = (a*3)+1
+            steps += 1
+
+    y.append(steps)
+
+plt.scatter(x, y, label='circle', color='red', marker='*', s=25)
+plt.xlabel('Integer ---->')
+plt.ylabel('Steps occured ---->')
+plt.title('Collatz Conjecture')
+plt.show()
