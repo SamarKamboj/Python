8056252616f05c54778a598bbd14cadb0720b676
Samar <52859266+SamarKamboj@users.noreply.github.com>
2022-07-28 22:15:10 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -9,20 +9,20 @@ def solve():
     arr = [int(i) for i in input().split()]
 
 
-    while True:
-        s = 0
+    #while True:
+        #s = 0
         for i in range(arr_len-1):
             smaller = 0
-            for j in range(1+i, arr_len):
-                if arr[i] & arr[j] != 0:
+            for j in range(0, arr_len):
+                if i != j and arr[i] & arr[j] != 0:
                     smaller = j if arr[j] < arr[i] else smaller
 
             if i < smaller:
                 arr[i], arr[smaller]= arr[smaller], arr[i]
                 s += 1
 
-        if s == 0:
-            break
+        #if s == 0:
+            #break
 
     if sorted(arr) == arr:
         return "Yes"
